59603af236d87b65e741d81163d192a3198d48b6
unknown <vicentesamuel937@gmail.com>
2025-08-14 17:08:52 -0400

    nuevo commit

--- AppBlazor.Entities/RepresentanteFormCLS.cs
+++ AppBlazor.Entities/RepresentanteFormCLS.cs
@@ -5,13 +5,13 @@ namespace AppBlazor.Entities
     public class RepresentanteFormCLS
     {
         [Required(ErrorMessage = "El numero de empleado es requerido")]
-        [Range(0, int.MaxValue, ErrorMessage = "El numero de empleado debe ser positivo")]
-        public int Num_Empl { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "El numero de empleado debe ser positivo")]
+        public int Num_Empl { get; set; }   
         [Required(ErrorMessage = "El nombre es requerido")]
         [MaxLength(60, ErrorMessage = "La longitud maxima es 60 caracteres")]
-        [MinLength(5, ErrorMessage = "La longitud minima es de 6 caracteres")]
+        [MinLength(5, ErrorMessage = "La longitud minima es de 5 caracteres")]
         public string Nombre { get; set; } = string.Empty;
-        [Required(ErrorMessage = "La edad es requerido")]
+        [Required(ErrorMessage = "La edad es requerida")]
         [Range(18, 80, ErrorMessage ="La edad debe ser mayor o igual a 18 o no debe ser mayor a 80") ]
         public int Edad { get; set; }
         [Required(ErrorMessage = "El cargo es requerido")]
@@ -20,13 +20,13 @@ namespace AppBlazor.Entities
         [Required(ErrorMessage = "La fecha de contrato es requerida")]
         [DataType(DataType.Date, ErrorMessage = "Por favor ingresa una fecha válida")]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        [Range(typeof(DateTime), "01/01/1980", "12/31/9999", ErrorMessage = "La fecha de contrato debe ser después de 1980")]
+        [Range(typeof(DateTime), "1980-01-01", "9999-12-31", ErrorMessage = "La fecha de contrato debe ser después de 1980")]
         public DateTime FechaContrato { get; set; }
         [Required(ErrorMessage = "La cuota es requerida")]
-        [Range(0, int.MaxValue, ErrorMessage = "la cuota de empleado debe ser positivo")]
+        [Range(1, double.MaxValue, ErrorMessage = "La cuota de empleado debe ser positivo")]
         public double Cuota { get; set; }
-        [Required(ErrorMessage = "La venta es requerido")]
-        [Range(0, int.MaxValue, ErrorMessage = "La venta del empleado debe ser positivo")]
+        [Required(ErrorMessage = "La venta es requerida")]
+        [Range(1, double.MaxValue, ErrorMessage = "La venta del empleado debe ser positivo")]
         public double Venta { get; set; }
 
     }
--- AppBlazor.Test/RepresentanteFormCLSTest.cs
+++ AppBlazor.Test/RepresentanteFormCLSTest.cs
@@ -5,25 +5,40 @@ namespace AppBlazor.Test
 {
     public class RepresentanteFormCLSTest
     {
-        private List<ValidationResult> ValidaModelo(object modelo) 
+        private List<ValidationResult> ValidaModelo(object modelo)
         {
-            var resultado = new List<ValidationResult>();
+            var resultados = new List<ValidationResult>();
             var contexto = new ValidationContext(modelo, null, null);
-            Validator.TryValidateObject(modelo, contexto, resultado, true);
-            return resultado;
+            Validator.TryValidateObject(modelo, contexto, resultados, true);
+            return resultados;
         }
         [Fact]
         public void ValidacionCamposVacios()
         {
+            
             var representante = new RepresentanteFormCLS();
             var errores = ValidaModelo(representante);
-            Assert.Contains(errores, e => e.ErrorMessage!.Contains("El numero de empleado es requerido") || e.ErrorMessage !.Contains("El numero de empleado debe ser positivo"));
+       
+            
+            Assert.Contains(errores, e => e.ErrorMessage!.Contains("El numero de empleado es requerido") 
+            || e.ErrorMessage!.Contains("El numero de empleado debe ser positivo"));
+
             Assert.Contains(errores, e => e.ErrorMessage!.Contains("El nombre es requerido"));
-            Assert.Contains(errores, e => e.ErrorMessage!.Contains("La edad es requerido"));
+
+            Assert.Contains(errores, e => e.ErrorMessage!.Contains("La edad es requerida") 
+            || e.ErrorMessage!.Contains("La edad debe ser mayor o igual a 18 o no debe ser mayor a 80"));
+
             Assert.Contains(errores, e => e.ErrorMessage!.Contains("El cargo es requerido"));
-            Assert.Contains(errores, e => e.ErrorMessage!.Contains("La fecha de contrato es requerida"));
-            Assert.Contains(errores, e => e.ErrorMessage!.Contains("La cuota es requerida"));
-            Assert.Contains(errores, e => e.ErrorMessage!.Contains("La venta es requerido"));
+
+            Assert.Contains(errores, e => e.ErrorMessage!.Contains("La fecha de contrato es requerida") 
+            || e.ErrorMessage!.Contains("La fecha de contrato debe ser después de 1980"));
+
+            Assert.Contains(errores, e => e.ErrorMessage!.Contains("La cuota es requerida") 
+            || e.ErrorMessage!.Contains("La cuota de empleado debe ser positivo"));
+
+            Assert.Contains(errores, e => e.ErrorMessage!.Contains("La venta es requerida") 
+            || e.ErrorMessage!.Contains("La venta del empleado debe ser positivo"));
         }
+
     }
 }
