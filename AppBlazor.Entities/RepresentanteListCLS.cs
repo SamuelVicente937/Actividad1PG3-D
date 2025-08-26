d1c1a8aaa9f0c0a2531ddd6bd5c1b3c7ad47331d
samuel <vicentesamue937@gmail.com>
2025-08-26 00:59:19 -0400

    commit26deagostoPW3

---
+++ AppBlazor.Entities/RepresentanteListCLS.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppBlazor.Entities
+{
+    public class RepresentanteListCLS
+    {
+        public int Num_empl { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public int Edad { get; set; }
+        public string Cargo { get; set; } = null!;
+        public DateTime FechaContrato { get; set; }
+        public double Cuota { get; set; }
+        public double Venta { get; set; }
+
+    }
+}
