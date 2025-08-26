2ad807d26665c891bb5cec12a7e29db264dde27a
xd <xd>
2025-08-26 15:12:49 -0400

    commit 26 de agosto

--- AppBlazor.Client/Program.cs
+++ AppBlazor.Client/Program.cs
@@ -1,11 +1,12 @@
 using AppBlazor.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using AppBlazor.Client.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddScoped<RepresentanteService>();
 await builder.Build().RunAsync();
---
+++ AppBlazor.Client/Services/RepresentanteService.cs
@@ -0,0 +1,27 @@
+﻿using AppBlazor.Entities;
+
+namespace AppBlazor.Client.Services
+{
+    public class RepresentanteService
+    {
+        private List<RepresentanteListCLS> lista;
+        public RepresentanteService()
+        {
+            lista = new List<RepresentanteListCLS>();
+            lista.Add(new RepresentanteListCLS { Num_empl = 1, Nombre = "Juanito Perez", Edad = 25, Cargo = "Vendedor", FechaContrato = new DateTime(2006, 3, 1), Cuota = 10, Venta = 15 });
+            lista.Add(new RepresentanteListCLS { Num_empl = 2, Nombre = "Juanito Perez", Edad = 25, Cargo = "Vendedor", FechaContrato = new DateTime(2006, 3, 1), Cuota = 10, Venta = 15 });
+            lista.Add(new RepresentanteListCLS { Num_empl = 3, Nombre = "Juanito Perez", Edad = 25, Cargo = "Vendedor", FechaContrato = new DateTime(2006, 3, 1), Cuota = 10, Venta = 15 });
+            lista.Add(new RepresentanteListCLS { Num_empl = 4, Nombre = "Juanito Perez", Edad = 25, Cargo = "Vendedor", FechaContrato = new DateTime(2006, 3, 1), Cuota = 10, Venta = 15 });
+            lista.Add(new RepresentanteListCLS { Num_empl = 5, Nombre = "Juanito Perez", Edad = 25, Cargo = "Vendedor", FechaContrato = new DateTime(2006, 3, 1), Cuota = 10, Venta = 15 });
+        }
+        public List<RepresentanteListCLS> listarRepresentante() 
+        {
+            return lista;
+        }
+        public void eliminarRepresentante(int num_empl)
+        {
+            var listaQueda = lista.Where(p => p.Num_empl != num_empl).ToList();
+            lista = listaQueda;
+        }
+    }
+}
